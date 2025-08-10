d6b1919eed90e58066350dd4539c8e8913400841
Victor Hugo Campos <vh.miyashiro@gmail.com>
2025-08-10 18:06:03 -0300

    Update app-routing-module.ts

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,16 +1,19 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
-import { AppComponent } from './app'; 
-import { AppRoutingModule } from './app-routing-module';
 import { FilmesList } from './pages/filmes-list/filmes-list';
 import { FilmesForm } from './pages/filmes-form/filmes-form';
 
+const routes: Routes = [
+  { path: '', redirectTo: 'filmes', pathMatch: 'full' },
+  { path: 'filmes', component: FilmesList },
+  { path: 'filmes/novo', component: FilmesForm },
+  { path: 'filmes/:id/editar', component: FilmesForm },
+  { path: '**', redirectTo: 'filmes' }
+];
+
 @NgModule({
-  declarations: [AppComponent, FilmesList, FilmesForm],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, ReactiveFormsModule],
-  bootstrap: [AppComponent]
+  imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'enabled' })],
+  exports: [RouterModule]
 })
-export class AppModule {}
+export class AppRoutingModule {}
