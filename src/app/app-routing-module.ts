822c4cdbc18c2ffd73da5b7066c50622aba36a8a
Victor Hugo Campos <vh.miyashiro@gmail.com>
2025-08-10 16:36:41 -0300

    Update app-routing-module.ts

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -1,17 +1,7 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
-import { FilmesList } from './pages/filmes-list/filmes-list';
-import { FilmesForm } from './pages/filmes-form/filmes-form';
-
 const routes: Routes = [
   { path: '', redirectTo: 'filmes', pathMatch: 'full' },
-  { path: 'filmes', component: FilmesList },
-  { path: 'filmes/novo', component: FilmesForm },
-  { path: 'filmes/:id/editar', component: FilmesForm },
+  { path: 'filmes', component: FilmesListComponent },
+  { path: 'filmes/novo', component: FilmesFormComponent },
+  { path: 'filmes/:id/editar', component: FilmesFormComponent },
+  { path: '**', redirectTo: 'filmes' }
 ];
-
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
