55779a14008a0cf5e8fbd35714a8bc15a23bdbaf
Victor Hugo Campos <vh.miyashiro@gmail.com>
2025-08-10 18:13:13 -0300

    Update app-module.ts

--- src/app/app-module.ts
+++ src/app/app-module.ts
@@ -1,16 +1,7 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { ReactiveFormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing-module';
+import { Component } from '@angular/core';
 
-import { FilmesList } from './pages/filmes-list/filmes-list';
-import { FilmesForm } from './pages/filmes-form/filmes-form';
-
-@NgModule({
-  declarations: [AppComponent, FilmesList, FilmesForm],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule, ReactiveFormsModule],
-  bootstrap: [AppComponent]
-})
-export class AppModule {}
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.html',
+  styleUrls: ['./app.scss'] 
+export class AppComponent {}
